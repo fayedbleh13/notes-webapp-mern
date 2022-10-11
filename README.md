78a43d36a3e92c0b6747b5e5d318a4423453186e
Fayed Mauyag <faemauyag13@gmail.com>
2022-10-11 20:47:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# note-webapp-MERN-
+ A note web application using the mern stack technlogies
---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.note-api.com',
+    'https://note-api.com'
+]
+
+module.exports = allowedOrigins
