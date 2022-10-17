a2489e477562dd61d1dd432b19dc83dbd63813ff
Fayed Mauyag <faemauyag13@gmail.com>
2022-10-17 18:23:48 +0800

    added redux & rtk functionality

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './assets/css/index.css'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <BrowserRouter>
-         <Routes>
-            <Route path="/*" element={<App />} />
-         </Routes>
-      </BrowserRouter>    
-  </React.StrictMode>
+   <React.StrictMode>
+      <Provider store={store}>
+         <BrowserRouter>
+            <Routes>
+               <Route path="/*" element={<App />} />
+            </Routes>
+         </BrowserRouter>
+      </Provider>    
+   </React.StrictMode>
 )
