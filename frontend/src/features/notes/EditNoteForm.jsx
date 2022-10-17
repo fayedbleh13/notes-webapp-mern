ae46db4a2dfca1b2414f007643ecc0633f067b5b
Fayed Mauyag <faemauyag13@gmail.com>
2022-10-18 01:11:55 +0800

    added and updated multiple files for notes
    added edit, create and delete functionality

---
+++ frontend/src/features/notes/EditNoteForm.jsx
@@ -0,0 +1,158 @@
+import React, { useState, useEffect } from 'react'
+import { useUpdateNoteMutation, useDeleteNoteMutation } from './notesApiSlice'
+import { useNavigate } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSave, faTrashCan } from '@fortawesome/free-solid-svg-icons'
+
+const EditNoteForm = ({ note, users }) => {
+    
+    const [updateNote, {
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    }] = useUpdateNoteMutation()
+
+    const [deleteNote, {
+        isSuccess: isDelSuccess,
+        isError: isDelError,
+        error: delerror
+    }] = useDeleteNoteMutation()
+
+    const navigate = useNavigate()
+
+    const [title, setTitle] = useState(note.title)
+    const [text, setText] = useState(note.text)
+    const [completed, setCompleted] = useState(note.completed)
+    const [userId, setUserId] = useState(note.user)
+
+    useEffect(() => {
+        if (isSuccess || isDelSuccess) {
+            setTitle('')
+            setText('')
+            setUserId('')
+            navigate('/dash/notes')
+        }
+    }, [isSuccess, isDelSuccess, navigate])
+
+    const onTitleChanged = e => setTitle(e.target.value)
+    const onTextChanged = e => setText(e.target.value)
+    const onCompletedChanged = e => setCompleted(prev => !prev)
+    const onUserIdChanged = e => setUserId(e.target.value)
+
+    const canSave = [title, text, userId].every(Boolean) && !isLoading
+
+    const onSaveNoteClicked = async (e) => {
+        if (canSave) {
+            await updateNote({ id: note.id, user: userId, title, text, completed })
+        }
+    }
+
+    const onDeleteNoteClicked = async (e) => {
+        await deleteNote({ id: note.id })
+    }
+
+    const created = new Date(note.createdAt).toLocaleDateString('en-us',{ day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' })
+    const updated = new Date(note.updatedAt).toLocaleDateString('en-us', { day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' })
+
+    const options = users.map(user => {
+        return (
+            <option
+                key={user.id}
+                value={user.id}
+            >
+                {user.username}
+            </option>
+        )
+    })
+
+    const errClass = (isError || isDelError) ? "errmsg" : "offscreen"
+    const validTitleClass = !title ? "form__input--incomplete" : ''
+    const validTextClass = !text ? "form__input--incomplete" : ''
+
+    const errContent = (error?.data?.message || delerror?.data?.message) ?? ''
+    
+    const content = (
+        <>
+            <p className={errClass}>{errContent}</p>
+
+            <form className="form" onSubmit={e => e.preventDefault()}>
+                <div className="form__title-row">
+                    <h2>Edit Note #{note.ticket}</h2>
+                    <div className="form__action-buttons">
+                        <button 
+                            className="icon-button"
+                            title="Save"
+                            onClick={onSaveNoteClicked}
+                            disabled={!canSave}
+                        >
+                            <FontAwesomeIcon icon={faSave} />
+                        </button>
+                        <button 
+                            className="icon-button"
+                            title="Delete"
+                            onClick={onDeleteNoteClicked}
+                        >
+                            <FontAwesomeIcon icon={faTrashCan} />
+                        </button>
+                    </div>
+
+                    <label className="form__label" htmlFor="note-title">Title:</label>
+                    <input
+                        className={`form__input ${validTitleClass}`}
+                        id="note-title"
+                        name="title" 
+                        type="text"
+                        autoComplete='off'
+                        value={title}
+                        onChange={onTitleChanged}
+                    />
+
+                    <label className="form__label" htmlFor="note-text">Text:</label>
+                    <textarea
+                        className={`form__input form__input--text ${validTextClass}`} 
+                        name="text" 
+                        id="note-text"
+                        value={text}
+                        onChange={onTextChanged}
+                    />
+                    
+                    <div className="form__row">
+                        <div className="form__divider">
+                            <label className="form_label form__checkbox-container" htmlFor="note-completed"> 
+                                Work Complete:
+                                <input
+                                    className="form__checkbox"
+                                    id="note-completed"
+                                    name="completed" 
+                                    type="checkbox"
+                                    checked={completed}
+                                    onChange={onCompletedChanged} 
+                                />
+                            </label>
+                            <label className="form__label form__checkbox-container" htmlFor="note-username">Assigned to:</label>
+                            <select
+                                className="form_select" 
+                                name="username" 
+                                id="note-username"
+                                value={userId}
+                                onChange={onUserIdChanged}
+                            >
+                                {options}
+                            </select>
+                        </div>
+                        <div className='form__divider'>
+                            <p>Created At:<br />{created}</p>
+                            <p>Updated At:<br />{updated}</p>
+                        </div>
+                    </div>
+                </div>
+
+            </form>
+        </>
+    )
+
+    return content
+}
+
+export default EditNoteForm
--- frontend/src/features/users/EditUser.jsx
+++ frontend/src/features/users/EditUser.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux'
 import { selectUserById } from './usersApiSlice'
 import EditUserForm from './EditUserForm'
 
-const EditUser = ({ user }) => {
+const EditUser = () => {
    const { id } = useParams()
 
    const user = useSelector(state => selectUserById(state, id))
