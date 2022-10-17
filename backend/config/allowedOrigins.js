5f1a61a660ba866c63718fd0a9da39426915e387
Fayed Mauyag <faemauyag13@gmail.com>
2022-10-17 18:20:55 +0800

    Update allowedOrigins.js
    added frontend localhost

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'https://www.note-api.com',
-    'https://note-api.com'
+    'https://note-api.com',
+    'http://localhost:5173'
 ]
 
 module.exports = allowedOrigins
