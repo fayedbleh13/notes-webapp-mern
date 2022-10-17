0d2c6eb15cff496c14a08824455ff0957c2412e7
Fayed Mauyag <faemauyag13@gmail.com>
2022-10-18 01:08:16 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import EditNote from './features/notes/EditNote'
 import EditUser from './features/users/EditUser'
 import NewNote from './features/notes/NewNote' 
 import NewUserForm from './features/users/NewUserForm'
+import Prefetch from './features/auth/Prefetch'
 
 function App() {
   return (
@@ -20,21 +21,21 @@ function App() {
          <Route path="/" element={<Layout />}>
             <Route index element={<Public />} />
             <Route path="login" element={<Login />} />
-
-            <Route path="dash" element={<DashLayout />}>
-               <Route index element={<Welcome />} />
-               <Route path="notes">
-                  <Route index element={<NotesList />} />
-                  <Route path=":id" element={<EditNote />} />
-                  <Route path="new" element={<NewNote />} />
-               </Route>
-               <Route path="users">
-                  <Route index element={<UsersList />} />
-                  <Route path=":id" element={<EditUser />} />
-                  <Route path="new" element={<NewUserForm />} />
-               </Route>
-            </Route> {/* end dash */}
-
+            <Route element={<Prefetch />}>
+                <Route path="dash" element={<DashLayout />}>
+                    <Route index element={<Welcome />} />
+                    <Route path="notes">
+                        <Route index element={<NotesList />} />
+                        <Route path=":id" element={<EditNote />} />
+                        <Route path="new" element={<NewNote />} />
+                    </Route>
+                    <Route path="users">
+                        <Route index element={<UsersList />} />
+                        <Route path=":id" element={<EditUser />} />
+                        <Route path="new" element={<NewUserForm />} />
+                    </Route>
+                </Route> {/* end dash */}
+            </Route>
          </Route>
       </Routes>
     </div>
